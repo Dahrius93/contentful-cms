e219c588b9241eac79a035900365b32c24c5f54d
Dahrius93 <dariolauretta93@gmail.com>
2026-03-11 19:17:01 +0100

    projects done

--- src/Projects.jsx
+++ src/Projects.jsx
@@ -8,15 +8,28 @@ const Projects = () => {
   }
 
   return (
-    <div>
-      {projects.map((project) => (
-        <div key={project.id}>
-          <h2>{project.title}</h2>
-          <p>{project.url}</p>
-          {project.img && <img src={project.img} alt={project.title} />}
-        </div>
-      ))}
-    </div>
+    <section className="projects">
+      <div className="title">
+        <h2>projects</h2>
+        <div className="title-underline"></div>
+      </div>
+      <div className="projects-center">
+        {projects.map((project) => {
+          const { id, img, url, title } = project;
+          return (
+            <a
+              key={id}
+              href={url}
+              target="_blank"
+              rel="noreferrer"
+              className="project"
+            >
+              <img src={img} alt={title} className="img" />
+            </a>
+          );
+        })}
+      </div>
+    </section>
   );
 };
 
